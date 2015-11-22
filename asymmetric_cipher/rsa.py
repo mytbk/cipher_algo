8c98ab1eb879d3ffcf1118b4ed3fd3145b02606c
Iru Cai <mytbk920423@gmail.com>
2015-11-22 21:45:03 +0800

    fix extend_euclid, pep8

--- asymmetric_cipher/rsa.py
+++ asymmetric_cipher/rsa.py
@@ -11,6 +11,11 @@ def extend_euclid(a, x):
     # a = r0*q1+r1 => r1 = a-r0*q1 = x*(-q1)+a*(1+q0*q1)
     # r0 = r1*q2+r2 => r2 = r0-r1*q2
     x_orig = x
+    a = a % x
+
+    if a == 1:
+        return 1
+
     x0, a0 = 1, 0
     x1, a1 = 0, 1
     while True:
@@ -26,7 +31,7 @@ def extend_euclid(a, x):
 
 
 def gen_coprime_pair(x):
-# generate a number coprime with x, and its reverse
+    # generate a number coprime with x, and its reverse
     e = randint(2, x - 1)
     d = extend_euclid(e, x)
     while d == 0:
@@ -36,7 +41,7 @@ def gen_coprime_pair(x):
 
 
 def RSA_genkey():
-# generate RSA key
+    # generate RSA key
     r1 = randint(2 ** 480, 2 ** 512)
     r2 = randint(2 ** 480, 2 ** 512)
 
@@ -59,18 +64,18 @@ def RSA_encrypt(n, e, data):
 def test_extend_euclid(a, b):
     a_ = extend_euclid(a, b)
     if a_ == 0:
-        print ("Not coprime")
+        print("Not coprime")
     if (a_ * a) % b == 1:
-        print ("Test extend_euclid pass.")
+        print("Test extend_euclid pass.")
 
 if __name__ == "__main__":
     test_extend_euclid(4, 93)
     test_extend_euclid(12, 18)
     test_extend_euclid(324134, 1234567)
-    print ("Generating an RSA key")
+    print("Generating an RSA key")
     n, e, d = RSA_genkey()
     data = 0x12345678
     data_enc = RSA_encrypt(n, e, data)
     data_enc_dec = RSA_encrypt(n, d, data_enc)
     if (data == data_enc_dec):
-        print ("RSA test ok")
+        print("RSA test ok")
